fd9bdf089ac4394e24e804a463d3a6fc3f951e66
Christian Garbs <mitch@cgarbs.de>
2010-10-09 13:11:32 +0200

    fix RSS feeds

--- webCOMA.pl
+++ webCOMA.pl
@@ -1004,7 +1004,7 @@ sub rssfeed($)
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <rss version="2.0"
  xmlns:dc="http://purl.org/dc/elements/1.1/"
- xmlns:content="http://purl.org/rss/1.0/modules/content/">
+ xmlns:content="http://purl.org/rss/1.0/modules/content/"
  xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <atom:link href="$baseurl/$feedfile" rel="self" type="application/rss+xml" />
